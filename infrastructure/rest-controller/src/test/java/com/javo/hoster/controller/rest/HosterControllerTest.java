aff485b16b761c86b4d32bb3e7252a9b1113606a
Jerson Villano <jervilla@publicisgroupe.net>
2022-08-03 22:15:41 -0500

    Defining REST controller for application

---
+++ infrastructure/rest-controller/src/test/java/com/javo/hoster/controller/rest/HosterControllerTest.java
@@ -0,0 +1,25 @@
+package com.javo.hoster.controller.rest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(HosterController.class)
+class HosterControllerTest {
+
+    @Test
+    void checkAccess() {
+    }
+
+    @Test
+    void testCheckAccess() {
+    }
+
+    @Test
+    void respondAccess() {
+    }
+}
